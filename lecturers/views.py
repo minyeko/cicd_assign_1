a14551315ba9cd9cab383497241139743a27ce65
minyeko <minyeko@outlook.com>
2024-07-07 18:20:53 +1200

    bug fix

--- lecturers/views.py
+++ lecturers/views.py
@@ -4,4 +4,4 @@ from .models import lecture
 def lecturer_list(request):
     lecturers = lecture.objects.all()
     return render(request, 'lecturers/lecturer_list.html', {'lecturers': lecturers})
-# Create your views here.
+# Create your views here. Test
