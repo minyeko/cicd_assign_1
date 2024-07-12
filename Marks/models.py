a7de82714c0c9a428984617764f3a9e34bf72e7f
minyeko <minyeko@outlook.com>
2024-07-12 13:39:40 +1200

    bug fix

--- Marks/models.py
+++ Marks/models.py
@@ -6,6 +6,6 @@ class Mark(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course_name = models.CharField(max_length=100)
     mark = models.FloatField()
-    
+
     def __str__(self):
         return f"{self.student} - {self.course_name}: {self.mark}"
